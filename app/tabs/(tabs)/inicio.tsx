5f28e7dd6bb80e84fedc7b5df6c9885f372ed681
JIDE-73 <167656483+JIDE-73@users.noreply.github.com>
2026-02-06 00:44:57 -0800

    ajsute del color

--- app/tabs/(tabs)/inicio.tsx
+++ app/tabs/(tabs)/inicio.tsx
@@ -31,31 +31,30 @@ export default function Inicio() {
 
       <Box className="items-center">
         <HStack space="lg" reversed={false} className="mt-8">
-          
           <Box className="h-32 w-32 rounded-2xl justify-center items-center">
-            <Box className="h-24 w-24 bg-primary-600 rounded-2xl justify-center items-center">
-              <Grid3x3 size={50} color="#FFD700" />
+            <Box className="h-24 w-24 bg-secondary-500 rounded-2xl justify-center items-center">
+              <Grid3x3 size={50} color="#fff112" />
             </Box>
             <Text className="text-white text-center mt-12">Pisos</Text>
           </Box>
 
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
         </HStack>
         <HStack space="lg" reversed={false} className="mt-8">
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
         </HStack>
         <HStack space="lg" reversed={false} className="mt-8">
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
         </HStack>
         <HStack space="lg" reversed={false} className="mt-8">
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
-          <Box className="h-32 w-32 bg-primary-400 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
+          <Box className="h-32 w-32 bg-secondary-500 rounded-2xl" />
         </HStack>
       </Box>
     </Center>
